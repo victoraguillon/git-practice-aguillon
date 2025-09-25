994aa161674b304e207ce615c3f4d5d6a94c1a2f
Victor Aguillon <victormaguillono2b@gmail.com>
2025-09-25 18:13:32 -0400

    feat: agregar más mensajes al programa

---
+++ hola_mundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es: Victor Aguillon" << endl;
+    cout << "Estoy aprendiendo Git en Programación II" << endl;
+    cout << "Este es mi segundo commit!" << endl;
+    return 0;
+ }   
